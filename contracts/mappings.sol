3c72a506c54f5e59962a8789ab57108589d12f31
Barkın Sarıkartal <43545365+barkinsarikartal@users.noreply.github.com>
2022-08-17 13:51:09 +0300

    Create mappings.sol

---
+++ contracts/mappings.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+contract constructors {
+
+    mapping(address => bool) public registeredUsers;
+    mapping(address => uint256) public favNum;
+
+    function register(uint256 _favNum) public {
+        require(!registeredUsers[msg.sender], "Kayit yok");
+        registeredUsers[msg.sender] = true;
+        favNum[msg.sender] = _favNum;
+    }
+
+    function deleteRegister() public {
+        require(registeredUsers[msg.sender]);
+        delete(registeredUsers[msg.sender]);
+        delete(favNum[msg.sender]);
+    }
+
+}
+
+contract NestedMapping {
+
+    mapping(address => mapping(address => uint256)) public debts;
+
+    function incDebts(address borrower, uint256 _debt) public {
+        debts[msg.sender][borrower] += _debt;
+    }
+
+    function decDebts(address borrower, uint256 _debt) public {
+        require(debts[msg.sender][borrower] >= _debt, "not enough debts");
+        debts[msg.sender][borrower] -= _debt;
+    }
+
+    function getDebt(address borrower) public view returns(uint256){
+        return debts[msg.sender][borrower];
+    }
+
+}
